d7822a4c87f920f36f68b6e78bbf9919e08028d1
amitasil <amit.rawat.1982@gmail.com>
2022-05-30 22:08:54 +1000

    AC2

--- tests/testWeatherDataBasedOnCoordinates.spec.ts
+++ tests/testWeatherDataBasedOnCoordinates.spec.ts
@@ -8,21 +8,16 @@ const longitude = -0.12768
 
 //Happy path Scenario
 test('should get current weather data based on longitude and latitude @lon&latTest', async ({ request }) => {
-    const response = await request.get(`forecast/hourly?lat=${latitude}&lon=${longitude}&key=${process.env.API_KEY}`)
+    const response = await request.get(`current?lat=${latitude}&lon=${longitude}&key=${process.env.API_KEY}`)
 
-    expect(response.status()).toBe(200)
+    await expect(response.status()).toBe(200)
 
     const jsonResponse = await response.json()
 
-    expect(jsonResponse.city_name).toEqual('London')
+    await expect(jsonResponse.data[0].city_name).toEqual('London')
 
-    expect(jsonResponse).toMatchObject({
-        city_name: 'London',
-        lon: -0.13,
-        timezone: 'Europe/London',
-        lat: 51.51,
-        country_code: 'GB',
-        state_code: 'ENG'
+    await expect(jsonResponse).toMatchObject({
+        count: 1
     })
 })
 
@@ -30,18 +25,22 @@ test('should get current weather data based on longitude and latitude @lon&latTe
 test('should get response code for not providing api key in URL @lon&latTest_UnAuthorized', async ({ request }) => {
     const response = await request.get(`forecast/hourly?lat=${latitude}&lon=${longitude}`)
 
-    expect(response.status()).toBe(403)
+    await expect(response).not.toBeOK()
+    
+    await expect(response.status()).toBe(403)
 })
 
 //Test with invalid latitude value
 test('should get response code 400 and error message for invalid lat/lon value @lon&latTest_InvalidLat', async ({ request }) => {
     const response = await request.get(`forecast/hourly?lat=invalidLat&lon=${longitude}&key=${process.env.API_KEY}`)
 
-    expect(response.status()).toBe(400)
+    await expect(response).not.toBeOK()
+
+    await expect(response.status()).toBe(400)
 
     const jsonResponse = await response.json()
 
-    expect(jsonResponse).toMatchObject({
+    await expect(jsonResponse).toMatchObject({
         error: 'Invalid lat/lon supplied.'
     })
 })
